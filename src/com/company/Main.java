173a51ef46bc90b6dc9c1faf8776dce86d44b2b2
macbookpro <abenov2005.uzer@gmail.com>
2020-03-22 02:52:25 +0600

    BIG correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,35 @@ public class Main {
 
         bigDictionary.put("робот", new String[]{"андроид", "киборг", "машина", "устройство"});
         bigDictionary.put("демон", new String[]{"бес", "нечистый", "дух", "дьявол"});
-        bigDictionary.put("нинздя", new String[]{"скрытный", "шпион", "убийца", "разведчик"});
+        bigDictionary.put("ниндзя", new String[]{"скрытный", "шпион", "убийца", "разведчик"});
         bigDictionary.put("добрый", new String[]{"добродушный", "благодушный", "добродетельный", "благой"});
         bigDictionary.put("харизматичный", new String[]{"прекрасный", "притягательный", "отличный", "симпатичный"});
         bigDictionary.put("красивый", new String[]{"блестящий", "дивный", "чудный", "обворожительный"});
+
+        HashMap<String, String[]> bigDictionaryHash = new HashMap<>(bigDictionary);
+
+        Set<String> stringSet = bigDictionary.keySet();
+        Iterator<String> iterator = stringSet.iterator();
+        while (iterator.hasNext()) {
+            try {
+                String key = iterator.next();
+                String[] array1 = bigDictionaryHash.get(key);
+                bigDictionaryHash.put(key, array1);
+
+                for (String string : array1) {
+                    ArrayList<String> arrayList = new ArrayList<>(array1.length);
+                    arrayList.addAll(Arrays.asList(array1));
+                    arrayList.add(key);
+                    arrayList.remove(string);
+                    String[] array2 = new String[arrayList.size()];
+                    //array2 = new arrayList.toArray(array2);
+                    bigDictionaryHash.put(string, array2);
+                    System.out.println("Большой словарь" + " - " + string + " - " + Arrays.toString(array1));
+                }
+            } catch (Exception e) {
+
+            }
+        }
 
         while (true) {
             System.out.println("Введите слово: ");
